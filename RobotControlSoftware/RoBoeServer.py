ff466c2008fb7515366e739218d377c7d40e291d
garetWk <gwkaiser@wlcl222105.wlcl.siu.edu>
2015-04-02 10:20:48 -0500

    server updates
    Changed code to use select to check socket statuses. Server now stays
    running even if client disconnects. client can the reconnect when
    needed.

--- RobotControlSoftware/RoBoeServer.py
+++ RobotControlSoftware/RoBoeServer.py
@@ -9,12 +9,12 @@ import struct
 
 import select
 
-serverIP = '10.88.211.244'
+serverIP = '10.88.222.105'
 serverPort = 50008
 
 usbport = '/dev/tty.usbmodemfa131'
 
-ser = serial.Serial(usbport,115200,timeout=None)
+#ser = serial.Serial(usbport,115200,timeout=None)
 
 
 
@@ -41,114 +41,118 @@ if __name__ == '__main__':
     print("started")
     print("at host: " + str(serverIP) + "port: " + str(serverPort))
 
-    try:
-        connection, address = serverTCP.accept()
-        '''
-        ssl_connection = ssl.wrap_socket(connection,
-                                         keyfile="server.key",
-                                         certfile="server.crt",
-                                         server_side=True,
-                                         cert_reqs=ssl.CERT_NONE,
-                                         ssl_version=ssl.PROTOCOL_SSLv23,
-                                         ca_certs="client.crt",
-                                         do_handshake_on_connect=True,
-                                         suppress_ragged_eofs=True,
-                                         ciphers=None)
-        '''
-
-    except Exception as e:
-        log = open("log.txt", 'a')
-        #msg = '\n' + 'SSL Error on ' + str(gmtime()) + ' by ' + address + '\n'
-        #log.write(msg)
-        log.write(str(e))
-        log.write('\n')
-        log.close()
-        raise e
-
-    '''
-    print (repr(ssl_connection.getpeername()))
-    print (ssl_connection.cipher())
-    print (pprint.pformat(ssl_connection.getpeercert()))
-    '''
-    print('Server connected by', address,'at', gmtime())
+    inputs = [ serverTCP ]
+    outputs = [ ]
     
+    while inputs:
+        readable, writeable, exceptional = select.select(inputs, outputs, inputs)
 
-    while True:
-        try:
-            ready_to_read, read_to_write, in_error = \
-                           select.select([connection,], [connection,], [], 5)
-        except select.error:
-            connection.shutdown(2)
-            connection.close()
-            print("closed connection")
+        for s in exceptional:
+            print('handling exceptional condition for ', s.getpeername())
+            inputs.remove(s)
+            if s in outputs:
+                outputs.remove(s)
+            s.close()
 
-        if len(ready_to_read) > 0:
-            
-            msg = connection.recv(11).decode()
-            print(msg)
-            msg = msg.split('-')
-            #print(msg)
+        for s in readable:
+            if s is serverTCP:
+                try:
+                    connection, address = s.accept()
+                    print('Server connected by', address,'at', gmtime())
+                    inputs.append(connection)
+                    '''
+                    ssl_connection = ssl.wrap_socket(connection,
+                                             keyfile="server.key",
+                                             certfile="server.crt",
+                                             server_side=True,
+                                             cert_reqs=ssl.CERT_NONE,
+                                             ssl_version=ssl.PROTOCOL_SSLv23,
+                                             ca_certs="client.crt",
+                                             do_handshake_on_connect=True,
+                                             suppress_ragged_eofs=True,
+                                             ciphers=None)
+                    '''
+                    
+                except Exception as e:
+                    log = open("log.txt", 'a')
+                    #msg = '\n' + 'SSL Error on ' + str(gmtime()) + ' by ' + address + '\n'
+                    #log.write(msg)
+                    log.write(str(e))
+                    log.write('\n')
+                    log.close()
+                    raise e
+            else:
+                msg = s.recv(11).decode()
+                if msg:
+                    print(msg)
+                    msg = msg.split('-')
+                    #print(msg)
+                    '''
+                    if msg[0] == 'e':
+                        ser.write(chr(16))
+                        ser.write('e')
+                        ser.write(chr(int(msg[1])))
+                        ser.write(chr(4))
+                        ser.write(chr(4))
+                        ser.write(chr(4))
 
+                    
+                    if msg[0] == 'r':
+                        ser.write(chr(16))
+                        ser.write('r')
+                        ser.write(chr(int(msg[1])))
+                        ser.write(chr(4))
+                        ser.write(chr(4))
+                        ser.write(chr(4))
 
-            if msg[0] == 'e':
-                ser.write(chr(16))
-                ser.write('e')
-                ser.write(chr(int(msg[1])))
-                ser.write(chr(4))
-                ser.write(chr(4))
-                ser.write(chr(4))
+                    if msg[0] == 'x':
+                        ser.write(chr(16))
+                        ser.write('x')
+                        ser.write(chr(int(msg[1])))
+                        ser.write(chr(4))
+                        ser.write(chr(4))
+                        ser.write(chr(4))
+                        
+                        
 
-            
-            if msg[0] == 'r':
-                ser.write(chr(16))
-                ser.write('r')
-                ser.write(chr(int(msg[1])))
-                ser.write(chr(4))
-                ser.write(chr(4))
-                ser.write(chr(4))
+                    if msg[0] == 'g':
+                        ser.write(chr(16))
+                        ser.write('g')
+                        ser.write(chr(int(msg[1])))
+                        ser.write(chr(4))
+                        ser.write(chr(4))
+                        ser.write(chr(4))
 
-            if msg[0] == 'x':
-                ser.write(chr(16))
-                ser.write('x')
-                ser.write(chr(int(msg[1])))
-                ser.write(chr(4))
-                ser.write(chr(4))
-                ser.write(chr(4))
-                
-                
+                    if msg[0] == 'w':
+                        ser.write(chr(16))
+                        ser.write('w')
+                        ser.write(chr(int(msg[1])))
+                        if msg[2] == 'f':
+                            ser.write('f')
+                        elif msg[2] == 'r':
+                            ser.write('r')
+                        ser.write(chr(int(msg[3])))
+                        ser.write(chr(4))
+                        
 
-            if msg[0] == 'g':
-                ser.write(chr(16))
-                ser.write('g')
-                ser.write(chr(int(msg[1])))
-                ser.write(chr(4))
-                ser.write(chr(4))
-                ser.write(chr(4))
-
-            if msg[0] == 'w':
-                ser.write(chr(16))
-                ser.write('w')
-                ser.write(chr(int(msg[1])))
-                if msg[2] == 'f':
-                    ser.write('f')
-                elif msg[2] == 'r':
-                    ser.write('r')
-                ser.write(chr(int(msg[3])))
-                ser.write(chr(4))
-                
-
-            if msg[0] == 'b':
-                ser.write(chr(16))
-                ser.write('b')
-                if msg[1] == 'f':
-                    ser.write('f')
-                elif msg[1] == 'r':
-                    ser.write('r')
-                ser.write(chr(int(msg[2])))
-                ser.write(chr(4))
-                ser.write(chr(4))
-
-            
+                    if msg[0] == 'b':
+                        ser.write(chr(16))
+                        ser.write('b')
+                        if msg[1] == 'f':
+                            ser.write('f')
+                        elif msg[1] == 'r':
+                            ser.write('r')
+                        ser.write(chr(int(msg[2])))
+                        ser.write(chr(4))
+                        ser.write(chr(4))
+                    '''
+                   
+                else:
+                    print('closing connection ', address)
+                    if s in outputs:
+                        outputs.remove(s)
+                    inputs.remove(s)
+                    s.close()
                 
             
             
